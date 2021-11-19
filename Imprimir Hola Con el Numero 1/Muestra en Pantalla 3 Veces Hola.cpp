4027b4b5abe31cd2f980fc93c76310141b791339
arash <arash@ARASHITORA>
2021-11-19 01:11:41 -0400

    Add project files.

---
+++ Imprimir Hola Con el Numero 1/Muestra en Pantalla 3 Veces Hola.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i = 0; //Declaración de variable
+
+    while (i < 3) {
+        puts("hola\n");
+        i = i + 1;
+    }
+    return 0;
+}
